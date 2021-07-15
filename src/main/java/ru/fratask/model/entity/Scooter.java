03a353c8f2c56e5c81a940f4745b91a298460a75
Fratask <amir.akhatov99@gmail.com>
2021-07-15 22:39:16 +0300

    Created Scooter Entity

---
+++ src/main/java/ru/fratask/model/entity/Scooter.java
@@ -0,0 +1,60 @@
+package ru.fratask.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "scooters")
+public class Scooter {
+
+    @Id
+    @SequenceGenerator(name = "scooter_id_generator", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "scooter_id_generator")
+    private Long id;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "charge_level")
+    private Integer chargeLevel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Integer getChargeLevel() {
+        return chargeLevel;
+    }
+
+    public void setChargeLevel(Integer chargeLevel) {
+        this.chargeLevel = chargeLevel;
+    }
+}
